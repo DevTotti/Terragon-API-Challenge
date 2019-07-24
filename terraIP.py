26b7d3d14673c641e7fe0dbe36b2f567f99c1a65
DevTotti <devtotti@gmail.com>
2019-07-24 21:37:35 +0100

    pushing new files to repository

---
+++ terraIP.py
@@ -0,0 +1,67 @@
+from flask import Flask, render_template, request
+import requests, redis, json
+
+conn = redis.StrictRedis(decode_responses=True)
+
+app = Flask(__name__)
+
+@app.route('/temperature', methods=['POST'])
+def temperature():
+    IP_Address = request.form['zip']
+    newDataBase = "terrisstifwefuwinwwegoiw"
+    if conn.exists(newDataBase) == 1:
+    	print("Database exist")
+    	newData = conn.hmget(newDataBase, "ip")
+    	if str(IP_Address) in newData:
+    		print("Data exist in database")
+    		existingData = conn.hmget(newDataBase, "ip", "continent_name", "country_name", "region_name", "latitude", "longitude")
+    		ip_add = existingData[0];ip_cont = existingData[1];ip_count = existingData[2];ip_state = existingData[3];ip_lat = existingData[4];ip_long = existingData[5]
+
+    	else:
+    		print("Data do not exist in database")
+    		json_object = cleanJson(IP_Address)
+    		json_object = json.dumps(json_object).replace("null",'"NIL"')
+    		json_object = json.loads(json_object)
+    		print(json_object)
+
+    		newData = conn.hmset(newDataBase, json_object)
+    		ip_add = json_object['ip'];ip_cont = json_object['continent_name'];ip_count = json_object['country_name'];ip_state = json_object['region_name'];ip_lat = json_object['latitude'];ip_long = json_object['longitude']
+
+    	return render_template('temperature.html', temp=ip_add, cont=ip_cont, count=ip_count, lati=ip_lat, longi=ip_long, state=ip_state)
+
+
+    else:
+    	print("Database do not exist")
+    	json_ = cleanJson(IP_Address)
+    	json_ = json.dumps(json_).replace("null",'"NIL"')
+
+    	try:
+    		json_object = json.loads(json_)
+    		print(json_object)
+    		newData = conn.hmset(newDataBase, json_object)
+    		print("Done")
+    	except Exception as error:
+    		print("Error here: ",error)
+
+    	return json_
+
+
+def cleanJson(IP_Address):
+	r = requests.get('http://api.ipstack.com/'+IP_Address+'?access_key=024e3f392effb6c46a4003ad68617a4f')
+	json_object = r.json()
+	del json_object["location"]
+	delete = ("zip","type","continent_code","country_code","region_code")
+
+	for rmv in delete:
+		json_object.pop(rmv)
+		pass
+
+
+	return json_object
+
+@app.route('/')
+def index():
+	return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(host="localhost", port=5001, debug=True)
